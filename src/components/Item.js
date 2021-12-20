274609c9e31a80ace70dba146634da2bd8e34aaa
Dakota Castle <barrettcastle@gmail.com>
2021-12-20 17:00:29 -0500

    commit tree c9ed7bf38e5ab142a8838ade63d861bce970a0b1

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [addCart, setCart]=useState(true)
+  
+function handleCart(){
+  setCart(!addCart)
+}
   return (
-    <li className="">
+    <li className={!addCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{addCart ? "Add to" : "Remove from"}Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setCat] = useState('All')
+
+  function catChange(e){
+    setCat(e.target.value)
+  }
+  const filterItems = items.filter(item => {
+    if (selectedCategory === "All"){
+      return true
+    } else {
+      return item.category === selectedCategory
+    }
+  })
+  const cartItems = filterItems.map((item) => (
+    <Item key={item.id}
+    name={item.name}
+    category={item.category} />
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={catChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +31,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {cartItems}
       </ul>
     </div>
   );
